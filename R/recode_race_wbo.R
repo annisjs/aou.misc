be91a146cab7395b57c7039605ecedcbfac558b5
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2024-02-22 14:54:11 -0600

    redefining other missing/other

--- R/recode_race_wbo.R
+++ R/recode_race_wbo.R
@@ -4,8 +4,8 @@
 recode_race_wbo <- function(race)
 {
   ifelse(race == "I prefer not to answer" |
-         race == "None of these" |
-         race == "None Indicated",
+         race == "None Indicated" |
+         race == "PMI: Skip",
        NA,
        ifelse(race == "Black or African American",
               "Black",
