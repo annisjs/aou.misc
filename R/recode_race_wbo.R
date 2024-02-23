b345c323d24864ce7badd4599bdc844b6f76ec42
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2024-02-23 10:18:10 -0600

    Adding verbose mapping statement for race recoding function

--- R/recode_race_wbo.R
+++ R/recode_race_wbo.R
@@ -3,13 +3,21 @@
 #' @export
 recode_race_wbo <- function(race)
 {
-  ifelse(race == "I prefer not to answer" |
-         race == "None Indicated" |
-         race == "PMI: Skip",
-       NA,
-       ifelse(race == "Black or African American",
-              "Black",
-              ifelse(race == "White",
-                     "White",
-                     "Other")))
+    cat("Found the following levels for ")
+    print(table(race))
+    cat("\nMapping:")
+    cat("\nI prefer not to answer    -> NA")
+    cat("\nNone Indicated            -> NA")
+    cat("\nPMI: Skip                 -> NA")
+    cat("\nBlack or African American -> Black")
+    cat("\nWhite                     -> White")
+    cat("\nDefault is 'Other'")
+    data.table::fcase(race == "I prefer not to answer"    , as.character(NA)
+                      ,race == "None Indicated"           , as.character(NA)
+                      ,race == "PMI: Skip"                , as.character(NA)
+                      ,race == "Black or African American", "Black"
+                      ,race == "White"                    , as.character(NA)
+                      ,default =                            "Other"
+                      )
+
 }
