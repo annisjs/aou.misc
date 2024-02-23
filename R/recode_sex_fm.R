f189c3b8d4a667ed2f3ab92f3df54b8734a42252
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2024-02-23 10:08:44 -0600

    adding mapping statement when recoding sex

--- R/recode_sex_fm.R
+++ R/recode_sex_fm.R
@@ -3,10 +3,17 @@
 #' @export
 recode_sex_fm <- function(sex)
 {
-  ifelse(sex == "Not male, not female, prefer not to answer, or skipped" |
-           sex == "No matching concept",
-         NA,
-         ifelse(sex == "Male",
-                "Male",
-                "Female"))
+  cat("Found the following levels:")
+  print(table(sex))
+  cat("\nMapping:")
+  cat("\nNot male, not female, prefer not to answer, or skipped -> NA")
+  cat("\nNo matching concept                                    -> NA")
+  cat("\nMale                                                   -> Male")
+  cat("\nFemale                                                 -> Female")
+  cat("\nDefault is NA")
+  data.table::fcase(sex == "Not male, not female, prefer not to answer, or skipped", as.character(NA),
+                    sex == "No matching concept"                                   , as.character(NA),
+                    sex == "Male"                                                  , "Male",
+                    sex == "Female"                                                , "Female",
+                    default =                                                      as.character(NA))
 }
