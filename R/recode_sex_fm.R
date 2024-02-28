5bfdb69641cab2930157ec611590224db8c76338
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2024-02-28 15:34:26 -0600

    simplifying recode

--- R/recode_sex_fm.R
+++ R/recode_sex_fm.R
@@ -6,14 +6,10 @@ recode_sex_fm <- function(sex)
   cat("Found the following levels for ")
   print(table(sex))
   cat("\nMapping:")
-  cat("\nNot male, not female, prefer not to answer, or skipped -> NA")
-  cat("\nNo matching concept                                    -> NA")
-  cat("\nMale                                                   -> Male")
-  cat("\nFemale                                                 -> Female")
+  cat("\nMale   -> Male")
+  cat("\nFemale -> Female")
   cat("\nDefault is NA")
-  data.table::fcase(sex == "Not male, not female, prefer not to answer, or skipped", as.character(NA)
-                    ,sex == "No matching concept"                                  , as.character(NA)
-                    ,sex == "Male"                                                 , "Male"
-                    ,sex == "Female"                                               , "Female"
-                    ,default =                                                       as.character(NA))
+  data.table::fcase(sex == "Male"    , "Male"
+                    ,sex == "Female" , "Female"
+                    ,default =       as.character(NA))
 }
