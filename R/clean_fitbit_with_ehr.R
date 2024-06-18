232d4d7dbc0489340706af72e9eaa1ca206850bd
Cihan <adnancihan.cakar@vumc.org>
2024-06-18 14:10:54 -0400

    Update clean_fitbit_with_ehr.R

--- R/clean_fitbit_with_ehr.R
+++ R/clean_fitbit_with_ehr.R
@@ -11,7 +11,7 @@
 #' @export
 clean_fitbit_with_ehr <- function(output_folder="datasets", bucket=NULL)
 {
-  dat = multi_query(c("fitbit", "demographics", "wear_time", "last_medical_encounter"), output_folder=output_folder, bucket=bucket)
+  dat = multi_query(c("fitbit", "demographics", "wear_time", "last_medical_encounter"), output_folder=output_folder, bucket=bucket, load_queries=T)
   fitbit_dat <- clean_fitbit(dat$fitbit, dat$wear_time, dat$demographics[,c("person_id","date_of_birth")])
   fitbit_dat <- merge(fitbit_dat,dat$last_medical_encounter,by="person_id")
   fitbit_ehr_cohort <- fitbit_dat[,c("person_id")]
