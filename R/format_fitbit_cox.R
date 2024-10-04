d36985296bd31993412a08ae50f42584f92c41ec
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2024-10-04 12:42:23 -0500

    Cast to date

--- R/format_fitbit_cox.R
+++ R/format_fitbit_cox.R
@@ -23,7 +23,7 @@ format_fitbit_cox <- function(fitbit,dx,last_medical_encounter)
 
   merged_cox[, min_fitbit_date := min(date),.(person_id)]
   merged_cox <- merged_cox[order(date)]
-  merged_cox[, days_from_start := as.numeric(date - min_fitbit_date),.(person_id)]
+  merged_cox[, days_from_start := as.numeric(as.Date(date) - as.Date(min_fitbit_date)),.(person_id)]
   merged_cox[, month := cut(days_from_start,seq(0,100000,30),include.lowest=T,right=F,labels=F)]
   merged_cox[, time1 := as.Date(min_fitbit_date) + lubridate::days((month-1)*30),.(person_id)]
   merged_cox[, time2 := as.Date(time1) + lubridate::days(29),.(person_id)]
