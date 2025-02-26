b84655b465bff4d2740d7558039bc58a0345c1bf
Cakar, Adnan Cihan <adnancihan.cakar@vumc.org>
2025-02-26 22:24:49 +0000

    adds subset by window and days in window range

--- R/subset_by_date_window.R
+++ R/subset_by_date_window.R
@@ -6,18 +6,22 @@
 #' @param after time range in the future
 #' @param date_col date column for the subset 
 #' @param id_col grouping variable for anchoring
+#' @param return_all either to return all dates for the subset of patients or just in the given window
 #' @examples 
 #'\dontrun{
 #' fitbit_dat <- subset_by_date_window(fitbit_dat, "covid_date", c(100,100))
 #' }
 #' @import data.table
 #' @export
-subset_by_date_window <- function(dat, anchor_date, date_range, date_col="date",
-                                  id_col = "person_id")
+subset_by_date_window <- function(dat, anchor_date, before, after, date_col="date",
+                                       id_col = "person_id", return_all = TRUE)
 {
   dat[, max_date:=max(get(date_col)), .(get(id_col))]
   dat[, min_date:=min(get(date_col)), .(get(id_col))]
-  dat <- dat[(max_date - get(anchor_date) > date_range[[1]]) & (get(anchor_date) - min_date > date_range[[2]])]
+  dat <- dat[((get(anchor_date) - min_date) > before) & ((max_date - get(anchor_date)) > after)]
+  if(!return_all){
+    dat = dat[((get(anchor_date) - get(date_col))<before) & ((get(date_col) - get(anchor_date))<after)]
+  }
   dat[, min_date := NULL]
   dat[, max_date := NULL]
   dat
---
+++ R/subset_by_min_days_in_range.R
@@ -0,0 +1,31 @@
+#' Subsets time series data using a minimum of records during given period relative to an anchor column
+#' @param dat a data.table representing the time series data with at least the following columns person_id, date
+#' @param range_start start of time range - can be positive or negative
+#' @param range_end end of time range - can be positive or negative
+#' @param min_days_count minimum number of records to require for subsetting
+#' @param anchor_col_name the name of the column that is the anchor for the range
+#' @param return_all either to return all dates for the subset of patients or just in the given window
+#' @examples 
+#'\dontrun{
+#' fitbit_dat <- read_bucket("datasets/fitbit.csv")
+#' subset_by_min_days_in_range(fitbit_dat, -365,0, 180, date)
+#' }
+#' @import data.table
+#' @export
+subset_by_min_days_in_range = function(fitbit_dat, range_start, range_end, min_days_count, anchor_col_name, return_all=TRUE){
+  dat = fitbit_dat
+  dat[, tte := as.numeric(date - get(anchor_col_name))]
+  dat_in_range = dat[(tte >= range_start) & (tte <= range_end)]
+  if(!return_all){
+    dat = merge(dat_in_range, dat_in_range[, .(days_in_range = .N), .(person_id)],
+                by="person_id")
+  }else{
+    dat = merge(dat, dat_in_range[, .(days_in_range = .N), .(person_id)],
+                by="person_id")
+    
+  }
+  dat = dat[days_in_range >= min_days_count]
+  dat$tte = NULL
+  dat$days_in_range = NULL
+  return(dat)
+}
