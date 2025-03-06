5eb58a2ccf5785cf37252b5d041ebda679ab991d
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2025-03-06 08:42:22 -0600

    Fixing mappings

--- R/recode_edu.R
+++ R/recode_edu.R
@@ -6,7 +6,7 @@ recode_edu <- function(education_status)
   cat("Found the following levels for ")
   print(table(education_status))
   cat("\nMapping:")
-  cat("\nCollege graduate or advanced degree ->college")
+  cat("\nHighest Grade: College Graduate ->college")
   cat("\nHighest Grade: Five Through Eight ->no_college")
   cat("\nHighest Grade: Nine Through Eleven ->no_college")
   cat("\nHighest Grade: Twelve Or GED ->no_college")
@@ -18,7 +18,7 @@ recode_edu <- function(education_status)
   data.table::fcase(
         education_status == "PMI: Skip", as.character(NA),
         education_status == "PMI: Prefer Not To Answer", as.character(NA),
-        education_status == "College graduate or advanced degree","college",
+        education_status == "Highest Grade: College Graduate","college",
         education_status == "Highest Grade: Five Through Eight","no_college",
         education_status == "Highest Grade: Nine Through Eleven","no_college",
         education_status == "Highest Grade: Twelve Or GED","no_college",
